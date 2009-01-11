fc2e730fce7a9946121c9e8537524223c5c7be8e
bozhidar.bozhanov <bozhidar.bozhanov@1876a5f9-5a52-0410-a00d-6df5d22118a9>
2009-01-11 10:55:29 +0000

    Mavenizing

--- ticketsbg/src/main/java/com/tickets/client/constants/Messages.java
+++ ticketsbg/src/main/java/com/tickets/client/constants/Messages.java
@@ -1,9 +1,8 @@
 package com.tickets.client.constants;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.i18n.client.Constants;
 
-public interface Messages extends Constants {
+public interface Messages extends com.google.gwt.i18n.client.Messages {
     public static Messages m = (Messages) GWT.create(Messages.class);
 
     String tickets();
